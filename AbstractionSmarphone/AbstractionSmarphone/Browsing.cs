7fe522165c42415cd97b7aa4a1a6962e71f2abee
Lincchi <61933058+lincchi@users.noreply.github.com>
2020-06-11 19:40:15 +0700

    Add files via upload

---
+++ AbstractionSmarphone/AbstractionSmarphone/Browsing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace AbstractionSmarphone
+{
+    class Browsing : Fungction
+    {
+        public string Sites { get; set; }
+
+        public Browsing(string sites)
+        {
+            
+            NamaFungsi = "Browsing";
+            Sites = sites;
+        }
+        public override string Fungsinya()
+        {
+            return (Sites);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Situs yang dikunjungi {Sites}");
+        }
+    }
+}
---
+++ AbstractionSmarphone/AbstractionSmarphone/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractionSmarphone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fungction[] fungctions = { new Browsing("google") };
+            foreach (Fungction f in fungctions)
+            {
+                f.GetInfo();
+                
+
+                Browsing browsing = f as Browsing;
+                if (browsing == null)
+                {
+                    Console.WriteLine("Eror 404");
+                }
+                if (f is Browsing)
+                {
+                    Console.WriteLine("Memuat situs yang anda tuju");
+                }
+            }
+        }
+    }
+}
